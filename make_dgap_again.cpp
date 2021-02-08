9a04168e18cae38ec7324e1de56d267f4454464a
Tikhon-Volzhin <volzhin.ts@phystech.edu>
2021-02-08 18:05:56 +0300

    yes its true

---
+++ make_dgap_again.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+//minecraft is my life
+int main()
+{
+    std::cout << "Hello World!\n";
+	std::vector<int> array;
+	for (int i = 0; i < 100; i++) {
+		array.push_back(i);
+	}
+	return 0;
+}
