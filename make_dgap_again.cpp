c7c6b99418ed4c44e3d9680ebd8eb1108c6a0a02
Tikhon-Volzhin <volzhin.ts@phystech.edu>
2021-02-09 08:55:03 +0300

    hjfgkufkuf

--- make_dgap_again.cpp
+++ make_dgap_again.cpp
@@ -3,7 +3,7 @@
 //minecraft is my life
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 	std::vector<int> array;
 	for (int i = 0; i < 100; i++) {
 		array.push_back(i);
